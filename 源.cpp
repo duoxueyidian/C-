2518501e03ceb07cc21e892e9fa23112a6f9ccff
duoxueyidian <3095375699@qq.com>
2024-06-14 23:25:05 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -2,34 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-//arr-待排数组首地址，arrtmp-用于排序的临时数组的首地址
-//start-待排区间第一个元素位置，end-待排区间的最后一个元素位置
-void _mergesort(int* arr, int* arrtmp, int start, int end) {
-    //如果区间元素少于2，递归终止
-    if (start >= end) return;
-    int mid = start + (end - start) / 2;//计算排序区间的中间位置
-
-    int istart1 = start, iend1 = mid;//左区间的起始位置
-    int istart2 = mid + 1, iend2 = end;//右区间的起始位置
-
-    _mergesort(arr, arrtmp, istart1, iend1);//左区间递归
-    _mergesort(arr, arrtmp, istart2, iend2);//右区间递归
-
-    int ii = start;//arrtmp计数器
-    while (istart1 <= iend1 && istart2 <= iend2)
-        arrtmp[ii++] = arr[istart1] < arr[istart2] ? arr[istart1++] : arr[istart2++];
-    while (istart1 <= iend1)
-        arrtmp[ii++] = arr[istart1++];
-    while (istart2 <= iend2)
-        arrtmp[ii++] = arr[istart2++];
-    memcpy(arr + start, arrtmp + start, (end - start + 1) * sizeof(int));
-}
+int min(int x, int y) { return x < y ? x : y; }
 
 void MergeSort(int* arr, unsigned int n) {
     if (n < 2) return;
-    int *arrtmp=(int*)malloc(n*sizeof(int));
-    _mergesort(arr, arrtmp, 0, n - 1);//调用递归函数进行排序
-    free(arrtmp);
+    int *aa = arr;//aa指向待排序数组
+    int* bb = (int*)malloc(n * sizeof(int));//bb指向已排序的数组
+    
+    int iseg;//区间分段的计数器，1,2,4,8...
+    int istart;//区间起始位置的计数器
+
+    //排序趟数的循环
+    for (iseg = 1; iseg < n; iseg *= 2) {
+        //每趟排序选取区间的循环
+        for (istart = 0; istart < n; istart += iseg * 2) {
+            //把每个区间分成两部分，ilow是起始位置，imid是中间位置，imax是结束位置
+            int ilow = istart;
+            int imid = min(istart + iseg, n);
+            int imax = min(istart + iseg * 2, n);
+
+            int ii = ilow;//已排序数组的计数器
+            int istart1 = ilow, iend1 = imid;
+            int istart2 = imid, iend2 = imax;
+
+            //把待排序左右两边数列合并到已排序数组
+            while ((istart1 < iend1) && (istart2 < iend2))
+                bb[ii++] = aa[istart1] < aa[istart2] ? aa[istart1++] : aa[istart2++];
+            while (istart1 < iend1) bb[ii++] = aa[istart1++];
+            while (istart2 < iend2) bb[ii++] = aa[istart2++];
+        }
+        //交换一下两个数组的指针，准备下一趟排序
+        int* ptmp = aa; aa = bb; bb = ptmp;
+    }
+    //如果aa指向的不是原始数组的指针，把aa中的内容复制到arr中
+    if (aa != arr) {
+        memcpy(arr, aa, n * sizeof(int));
+        bb = aa;
+    }
+    free(bb);
 }
 
 int main() {
